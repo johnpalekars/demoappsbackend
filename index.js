7e989ab4645c05ac1b03ff3238d47fa4f766e40b
Abhishek Palekar <johnpalekars@gmail.com>
2022-10-03 23:41:40 +0530

    some socket listener code

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const http = require('http');
-const express = require('express');
-const cors = require('cors');
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const PORT = 4000;
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-server.listen(PORT, () => 'listning on port' + PORT);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to server " + socket.id);
+
+  socket.on("join_room", (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room);
+
+    console.log(username);
+  });
+});
+
+server.listen(PORT, () => "listning on port" + PORT);
